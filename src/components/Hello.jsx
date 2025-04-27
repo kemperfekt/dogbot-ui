7cfdbd57ab9a803517c6feed87cc53c5123f45cb
nasenblick <info@nasenblick.de>
2025-04-27 22:27:34 +0200

    Downgrade to tailwaind v3.x

--- src/components/Hello.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/Hello.jsx
-export default function Hello() {
-    return (
-      <div style={{ padding: "2rem", fontSize: "2rem", textAlign: "center" }}>
-        🎉 DogBot UI läuft!
-      </div>
-    );
-  }
-  
