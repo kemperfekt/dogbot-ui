4eeef7d0c8d53bb1d1288d6f407f6a3dd69ad76b
Kemperfekt <pk@kemperfekt.de>
2025-05-30 12:53:14 +0200

    revert back to old color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,15 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
       extend: {
+
         colors: {
-          primary: '#184567',//'#4B7893',
+          primary: '#4B7893',
           accent: '#C45C2E',
-          background: '#FBEED5',//'#F7E5C9',
+          background: '#F7E5C9',
           secondary: '#CD8E3A',
           highlight: '#B86C26',
         },
+      
         fontFamily: {
           sans: ['Figtree', 'sans-serif'],
         },
